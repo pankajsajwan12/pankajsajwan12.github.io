3b977837228cc0fb9fc407f71d344cd39cdaba3d
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2023-01-10 14:54:58 +0530

    change some website live link

--- src/components/portifilo/Portifilo.jsx
+++ src/components/portifilo/Portifilo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import img2 from '../../my-assets/img2.jpg'
+import monster from '../../my-assets/monster_project.png'
 import './portifilo.css'
 import img3 from '../../my-assets/img3.jpg'
 
@@ -14,7 +14,7 @@ const Portifilo = () => {
 
       <article className="portfolio_item">
           <div className="portfolio_item-image">
-              <img src="https://raw.githubusercontent.com/pankajsajwan12/skinny-pot-4052/main/monster/public/homepage-1.png" alt=""  className="portfolio-img"/>
+              <img src={monster} alt=""  className="portfolio-img"/>
           </div>
           <div className="protfolio_item-image_Tech_Stack">
           <p>Monster-jobs is a source for jobs and career opportunities. Search for jobs, read career advice from Monster's job experts, and find hiring and recruiting ...</p>
@@ -23,7 +23,7 @@ const Portifilo = () => {
           </div>
           <div className="portfolio_item-cta">
           <a href="https://github.com/pankajsajwan12/skinny-pot-4052" className='btn'target="_blank">Github</a>
-          <a href="https://monsterindia.vercel.app/" className="btn btn-primary" target="_blank">Live</a>
+          <a href="https://monster-iota.vercel.app/" className="btn btn-primary" target="_blank">Live</a>
           </div>
           
         </article>
@@ -54,7 +54,7 @@ const Portifilo = () => {
           <h4>A collaborative project built by a team of 5 executed in 5 days.</h4>
           </div>
           <div className="portfolio_item-cta">
-          <a href="https://github.com/KalashThakur/straberrynet" className='btn'>Github</a>
+          <a href="https://github.com/KalashThakur/straberrynet" className='btn' target="_blank">Github</a>
           <a href="https://imaginative-praline-24d6d4.netlify.app/" className="btn btn-primary" target="_blank">Live</a>
 
           </div>
@@ -69,8 +69,8 @@ const Portifilo = () => {
           <h4><b>Tech-Stack : </b>&nbsp;HTML,CSS, Javascript, Git </h4>
           </div>
           <div className="portfolio_item-cta">
-          <a href="https://github.com/pankajsajwan12/zara-clone" className='btn'>Github</a>
-          <a href="https://polite-lamington-faa8d2.netlify.app/" className="btn btn-primary" target="_blank">Live Demo</a>
+          <a href="https://github.com/pankajsajwan12/zara-clone" className='btn' target="_blank">Github</a>
+          <a href="https://eloquent-semolina-66e2d9.netlify.app/" className="btn btn-primary" target="_blank">Live Demo</a>
           </div>
         </article>
 
